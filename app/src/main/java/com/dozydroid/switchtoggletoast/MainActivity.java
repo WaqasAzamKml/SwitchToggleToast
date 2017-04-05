697d625afcc01090d371d0304b2a37e032ba4bce
Muhammad Waqas <waqasazamkml@gmail.com>
2017-04-05 17:51:29 +0500

    Switch implemented

--- app/src/main/java/com/dozydroid/switchtoggletoast/MainActivity.java
+++ app/src/main/java/com/dozydroid/switchtoggletoast/MainActivity.java
@@ -2,12 +2,29 @@ package com.dozydroid.switchtoggletoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch mySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySwitch = (Switch) findViewById(R.id.switch1);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this, "Switch Checked", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Switch Unchecked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
